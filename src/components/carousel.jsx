b6f68a7f33a49fe853ad6fbbc924c568ed55bb58
nbhat1998 <nbhat1998@gmail.com>
2019-05-27 20:27:03 +0100

    reworked carousel with a better implementation from React bootstrap

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,55 +1,34 @@
 import React, { Component } from "react";
+import Carrysel from "react-bootstrap/Carousel";
 
 class Carousel extends Component {
   render() {
     // TODO: ensure that pictures are auto resized before they get here
     return (
-      <div
-        id="carouselExampleControls"
-        class="carousel slide"
-        data-ride="carousel"
-      >
-        <div class="carousel-inner">
-          <div class="carousel-item active">
+      <div className="carousel">
+        <Carrysel>
+          <Carrysel.Item>
             <img
-              class="d-block w-10"
-              src="https://t1.ftcdn.net/jpg/00/89/68/46/400_F_89684624_F5qxUjEEIcuXtXYVGjPE82gheEUbnF5t.jpg"
+              className="d-block w-10"
+              src="https://access.redhat.com/documentation/en-US/Fuse_ESB_Enterprise/7.1/html/EIP_Transaction_Guide/files/images/intro_to_txn_01.gif"
               alt="First slide"
             />
-          </div>
-          <div class="carousel-item">
+          </Carrysel.Item>
+          <Carrysel.Item>
             <img
-              class="d-block w-10"
-              src="https://i.pinimg.com/originals/b4/fd/44/b4fd4479457d483bbe4e81c9e12f1255.png"
-              alt="Second slide"
-            />
-          </div>
-          <div class="carousel-item">
-            <img
-              class="d-block w-10"
-              src="https://2.bp.blogspot.com/-y74wEPS8Sjk/V0YSARc67FI/AAAAAAAAFYM/OiKZdsh-DHcATfctP9gpatZRa_DaIYztwCLcB/s1600/1406296945663.jpg"
+              className="d-block w-10"
+              src="https://t1.ftcdn.net/jpg/00/89/68/46/400_F_89684624_F5qxUjEEIcuXtXYVGjPE82gheEUbnF5t.jpg"
               alt="Third slide"
             />
-          </div>
-        </div>
-        <a
-          class="carousel-control-prev"
-          href="#carouselExampleControls"
-          role="button"
-          data-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true" />
-          <span class="sr-only">Previous</span>
-        </a>
-        <a
-          class="carousel-control-next"
-          href="#carouselExampleControls"
-          role="button"
-          data-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true" />
-          <span class="sr-only">Next</span>
-        </a>
+          </Carrysel.Item>
+          <Carrysel.Item>
+            <img
+              className="d-block w-10"
+              src="https://static.businessinsider.com/image/52cd72fc69bedd5b311099ec-750.jpg"
+              alt="Third slide"
+            />
+          </Carrysel.Item>
+        </Carrysel>
       </div>
     );
   }
