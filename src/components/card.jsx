ef50c69ded4c53b36a3aaf4cbfc0a77adb1f9d36
nbhat1998 <nbhat1998@gmail.com>
2019-05-27 17:40:46 +0100

    removed images, will be readded in Carousel later!

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -6,11 +6,6 @@ class Card extends Component {
       <div class="card">
         <div class="card-body">
           <h5 class="card-title">Main particulars</h5>
-          <img
-            class="card-img-top"
-            src="http://writingexercises.co.uk/images2/randomimage/pipe-sculpture.jpg"
-            alt="card image cap"
-          />
           <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
           <p class="card-text">
             Some quick example text to build on the card title and make up the
